bd1762b806b175b974da807a961de18f09f0f67f
71200665williamchristian <79102775+71200665williamchristian@users.noreply.github.com>
2021-02-23 18:32:21 +0700

    Add files via upload

--- LAB-3-Struktur_Kontrol_dan_Percabangan.py
+++ LAB-3-Struktur_Kontrol_dan_Percabangan.py
@@ -14,20 +14,23 @@ print("Bangun datar yang dapat dihitung :")
 print("1. Persegi")
 print("2. Persegi panjang")
 print("3. Segitiga")
-pilihan = int(input("Masukkan pilihan anda : "))
-if pilihan == 1 :
-    sisi = float(input("Masukkan nilai sisi : "))
-    hasil_persegi = sisi * sisi
-    print("Luas persegi adalah",hasil_persegi)
-elif pilihan == 2 :
-    panjang = float(input("Masukkan nilai panjang : "))
-    lebar = float(input("Masukkan nilai lebar : "))
-    hasil_persegipanjang = panjang * lebar
-    print("Luas persegi panjang adalah",hasil_persegipanjang)
-elif pilihan == 3 :
-    alas = float(input("Masukkan nilai alas : "))
-    tinggi = float(input("Masukkan nilai tinggi : "))
-    luas_segitiga = 1/2 * alas * tinggi
-    print("Luas segitiga adalah",luas_segitiga)
-else :
-    print("Maaf kami hanya menyediakan pilihan diatas")
+try:
+    pilihan = int(input("Masukkan pilihan anda : "))
+    if pilihan == 1 :
+        sisi = float(input("Masukkan nilai sisi : "))
+        hasil_persegi = sisi * sisi
+        print("Luas persegi adalah",hasil_persegi)
+    elif pilihan == 2 :
+        panjang = float(input("Masukkan nilai panjang : "))
+        lebar = float(input("Masukkan nilai lebar : "))
+        hasil_persegipanjang = panjang * lebar
+        print("Luas persegi panjang adalah",hasil_persegipanjang)
+    elif pilihan == 3 :
+        alas = float(input("Masukkan nilai alas : "))
+        tinggi = float(input("Masukkan nilai tinggi : "))
+        luas_segitiga = 1/2 * alas * tinggi
+        print("Luas segitiga adalah",luas_segitiga)
+    else :
+        print("Maaf kami hanya menyediakan pilihan diatas")
+except:
+    print("Maaf yang anda input yang anda masukkan tidak valid")
